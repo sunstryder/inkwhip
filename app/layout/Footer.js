5ef71f7b0a14795fcdb26f9acc37ac3b8771c393
Tony Sun <tony.sun@crownbet.com.au>
2018-10-03 16:48:28 +1000

    add Footer component

---
+++ app/layout/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => (
+    <footer className="footer">
+        <div className ="container">
+            <div className="content has-text-centered">
+                <p>
+                    Built on <strong> React</strong> with <strong>Bulma</strong> by myself, <a href="https://github.com/sunstryder">Tony Sun</a>
+                <br />
+                Copyright 2018
+                </p>
+            </div>
+        </div>
+    </footer>
+)
+
+export default Footer

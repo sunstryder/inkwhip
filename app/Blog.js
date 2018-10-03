e8551b80014d57832273cb05f0c771bcd26beea8
Tony Sun <tony.sun@crownbet.com.au>
2018-10-03 16:54:25 +1000

    add a blog and home page to the app

---
+++ app/Blog.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Blog = () => (
+    <p>This is the Blog Page</p>
+)
+
+export default Blog
---
+++ app/Home.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Home = () => (
+    <p>This is the Home Page</p>
+)
+
+export default Home

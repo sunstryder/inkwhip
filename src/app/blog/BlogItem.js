624f9637bce1347e59494c62c312291169540d35
Tony Sun <tony.sun@crownbet.com.au>
2018-10-04 17:19:31 +1000

    - add react markdown to display blogitems correctly. - expand BlogItem to show date, icon, markdown and also link back to blog.

--- src/app/blog/BlogItem.js
+++ src/app/blog/BlogItem.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import * as Markdown from 'react-markdown'
+import moment from 'moment'
 
 const BlogItem = (props) => {
     return (
-        <div className="box content">
-            <h1>{props.title}</h1>
-            <p>{props.content}</p>
+        <div className="box">
+            <article className="media">
+                <div className="media-left">
+                    <figure className="image is-64x64">
+                        <img src={props.icon} alt="Image" />
+                    </figure>
+                </div>
+                <div className="media-content">
+                    <div className="content">
+                        <h1>{props.title}</h1>
+                        <Markdown 
+                            source={props.content.split(" ").splice(0,150).join(" ").concat('...')}
+                        />
+                    </div>
+                    <div className="level">
+                        <div className="level-left">
+                            <Link 
+                                className="level-item button is-small is-link is-outlined"
+                                to="/blog">
+                                Back to Blog
+                            </Link>
+                        </div>
+                        <div className="level-right">
+                            <p className="level-item has-text-link is-size-7">
+                                {moment(props.date).calendar(null, {
+                                    sameDay: '[Today]',
+                                    lastDay: '[Yesterday]',
+                                    lastWeek: '[Last] dddd',
+                                    sameElse: 'MMM Do YYYY'
+                                })}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </article>
         </div>
     )
 }
--- src/app/layout/Header.js
+++ src/app/layout/Header.js
@@ -36,7 +36,7 @@ class Header extends React.Component {
                   }}
                   src="https://media-exp2.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAU3AAAAJGE1MzYxNzYzLTE1NTUtNDEyYi04MzRjLTgzZjNkOGU0MGIzNg.jpg"
                   width="30px" alt="" />
-                <span>AaronKlaser.com</span>
+                <span>sunstryder</span>
               </NavLink>
               <button className="button navbar-burger" onClick={this.toggleNav}>
                 <span></span>
@@ -81,7 +81,7 @@ class Header extends React.Component {
                 </div>
               </div>
               <div className="navbar-end">
-                <a className="navbar-item" href="https://github.com/aaronklaser">
+                <a className="navbar-item" href="https://github.com/sunstryder">
                   <span className="icon">
                     <i className="fab fa-lg fa-github"></i>
                   </span>

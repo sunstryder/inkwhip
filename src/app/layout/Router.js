84bc264809b536391d63526c23d09d190c2ec703
Tony Sun <tony.sun@crownbet.com.au>
2018-10-05 12:09:13 +1000

    - Refactor BlogItem - Add component for Blog Post - Add shared components blog content and blog nav used by both item and post.
    misc:
    - update gitignore for webstorm

--- src/app/layout/Router.js
+++ src/app/layout/Router.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from './../Home'
 import Blog from './../Blog'
-import BlogPost from './../BlogPost'
+import BlogPost from '../blog/BlogPost'
 
 const Router = () => (
     <Switch>
